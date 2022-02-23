7bc4b5d91ab8cdc098cf410c76a8e9a004e5ccb4
Jens Meindertsma <mail@jensmeindertsma.com>
2022-02-23 14:49:05 +0100

    Remove CentOS support

--- ci/build-and-package.sh
+++ ci/build-and-package.sh
@@ -4,9 +4,13 @@ set -e
 
 echo "Building Volta"
 
-cargo build --release
+if [[ "$1" == *"apple-darwin"* ]]; then
+    SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path) MACOSX_DEPLOYMENT_TARGET=11.0 cargo build --release --target=$1
+else
+    cargo build --release --target $1
+fi
 
 echo "Packaging Binaries"
 
-cd target/release
+cd target/$1/release
 tar -zcvf "$1.tar.gz" volta volta-shim volta-migrate
